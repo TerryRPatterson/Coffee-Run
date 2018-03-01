0207621f405a5ff7d240adfe7d14e074ee77e22f
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-03-01 13:22:15 -0500

    Finished Server intergration

--- main.js
+++ main.js
@@ -1,69 +1,76 @@
-/*global  */
-/*eslint no-console: 0*/
-let history = window.localStorage;
-if (!history.getItem("currentOrder")){
-    history.setItem("currentOrder",JSON.stringify(0));
-    history.setItem("orders",JSON.stringify([]));
-}
-let form = document.querySelector(".form-container");
+/*global  $*/
+/*eslint no-console: 0
+no-unused-vars:0*/
+let orders = {};
+let server ="http://dc-coffeerun.herokuapp.com/api/coffeeorders";
 
+let form = document.querySelector(".form-container");
 
 
 let formSubmission =  function formSubmission(){
     let submission = {};
-    submission["Order Number"] = currentOrder;
     event.preventDefault();
-    submission["drink"] = form.querySelector("#coffeeOrder").value;
-    submission["email"] = form.querySelector("#emailInput").value;
-    form.querySelectorAll(".sizeSelector").forEach(function(item){
-        if (item.checked === true){
-            submission["size"] = item.value;
+    submission["coffee"] = form.querySelector("#coffeeOrder").value;
+    submission["emailAddress"] = form.querySelector("#emailInput").value;
+    form.querySelectorAll(".sizeSelector").forEach(function(order){
+        if (order.checked === true){
+            submission["size"] = order.value;
         }
     });
     submission["flavor"] = form.querySelector("#flavorShot").value;
-    submission["Caffeine"] = form.querySelector("#strengthLevel").value;
-    orders[currentOrder] = submission;
-    currentOrder ++;
+    submission["strength"] = form.querySelector("#strengthLevel").value;
+    $.post(server,submission);
+    setTimeout(function () {
+        $(".form-control").val("");
+    }, 2000);
     update();
 };
 
 let update = function update(){
     let orderDisplay = document.querySelector("#orderDisplay");
     orderDisplay.innerHTML = "";
-    if (orders.length > 0){
-        orders.forEach(function(item){
-            if (item){
-                let button = document.createElement("button");
-                button.setAttribute("label",`Remove Order ${item["Order Number"]}`);
-                button.textContent = `Remove Order ${item["Order Number"]}`;
-                button.classList.add("btn");
-                button.classList.add("btn-default");
-                let displayItem = document.createElement("li");
-                let text = "";
-                for (let entry in item){
-                    text += `${entry}: ${item[entry]} \n`;
+    $.get(server,function(data){
+        orders = data;
+
+        if (Object.keys(orders).length > 0){
+            for (let orderName in orders){
+                let order = orders[orderName];
+                if (order){
+                    let button = document.createElement("button");
+                    button.setAttribute("label",`Remove Order ${order["emailAddress"]}`);
+                    button.textContent = `Remove Order`;
+                    button.classList.add("btn");
+                    button.classList.add("btn-default");
+                    let displayItem = document.createElement("li");
+                    let text = "";
+                    for (let entry in order){
+                        if (order.hasOwnProperty(entry) && (entry !== "_id" &&
+                    entry !== "__v")){
+                            text += `${entry}: ${order[entry]} \n`;
+                        }
+                    }
+                    button.addEventListener("click",function(){
+                        setTimeout(function () {
+                            $.ajax({"method":"DELETE",
+                            "data":orderName})
+                            update();
+                        }, 2000);
+
+                    });
+                    displayItem.textContent = text;
+                    displayItem.setAttribute("id",order["Order Number"]);
+                    displayItem.appendChild(button);
+                    orderDisplay.appendChild(displayItem);
                 }
-                button.addEventListener("click",function(){
-                    delete orders[item["Order Number"]];
-                    update();
-                });
-                displayItem.textContent = text;
-                displayItem.setAttribute("id",item["Order Number"]);
-                displayItem.appendChild(button);
-                orderDisplay.appendChild(displayItem);
             }
-        });
-    }
-    else{
-        let displayItem = document.createElement("li");
-        displayItem.textContent = "No Orders";
-        orderDisplay.appendChild(displayItem);
-    }
-    history.setItem("currentOrder",JSON.stringify(currentOrder));
-    history.setItem("orders",JSON.stringify(orders));
+        }
+        else{
+            let displayItem = document.createElement("li");
+            displayItem.textContent = "No Orders";
+            orderDisplay.appendChild(displayItem);
+        }
+    });
 };
 
-let currentOrder = JSON.parse(history.getItem("currentOrder"));
-let orders = JSON.parse(history.getItem("orders"));
 update();
 form.addEventListener("submit",formSubmission);
